71cbff80e10a681dfda6951800b860ac0c8b41cd
sedaasatryan <138974989+sedaasatryan@users.noreply.github.com>
2023-08-27 10:21:36 +0400

    Update ssh_connect.sh

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Define remote server details
-REMOTE_HOST="139.144.71.186"
-REMOTE_USER="Maria"
+REMOTE_HOST="ip address"
+REMOTE_USER="name"
 
 #SSH command to connect to the remote server and execute ipblock.sh
 ssh "$REMOTE_USER"@"$REMOTE_HOST" '/path/to/myscript1.sh'
-REMOTE_HOST="139.144.71.186"
-REMOTE_USER="Maria"
+REMOTE_HOST="ip address"
+REMOTE_USER="name"
 ssh "$REMOTE_USER"@"$REMOTE_HOST" '/path/to/myscript1.sh'
